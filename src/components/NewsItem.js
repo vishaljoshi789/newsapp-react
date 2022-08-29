46ae53eaeca7ccc2675cd8f6f67d45cb512c43d0
Vishal <vishalwebdev0987@gmail.com>
2022-08-29 22:21:13 +0530

    test json created

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
     render() {
+        let { title, description } = this.props
         return (
             <div>
-                This is a news item
-            </div>
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href="/" className="btn btn-primary btn-sm">Read More </a>
+                    </div>
+                </div>
+            </div >
         )
     }
 }
